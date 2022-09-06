f96958b4b15b7367f32510623a2ee9b8e57c6422
LwFrahm <luke.2017@hotmail.com>
2022-09-06 09:00:07 -0500

    finished exercise

---
+++ exercises/Calculator/src/test/java/org/example/AppTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+
+
+    /**
+     * @return the suite of tests being tested
+     */
+
+
+    /**
+     * Rigourous Test :-)
+     */
+
+}
